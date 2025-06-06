42a669a6843bd683640e31476fe5431a797282a8
一秋 <1464074871@qq.com>
2025-06-06 15:27:25 +0800

    feat:添加选项配置

--- src/pages/HomeLayout/pages/FormDesigner/components/ComponentConfig/index.vue
+++ src/pages/HomeLayout/pages/FormDesigner/components/ComponentConfig/index.vue
@@ -69,25 +69,66 @@
             <div
               v-for="(option, index) in modelValue.options"
               :key="index"
-              class="flex gap-2"
+              class="flex gap-2 items-center"
             >
+              <!-- 默认值选择框 -->
+              <div class="flex-shrink-0">
+                <!-- 单选框类型使用 radio -->
+                <el-radio
+                  v-if="modelValue.type === 'radio' || modelValue.type === 'select'"
+                  :model-value="modelValue.defaultValue"
+                  :label="option.value"
+                  @change="setDefaultValue(option.value)"
+                  size="small"
+                >
+                </el-radio>
+                <!-- 多选框类型使用 checkbox -->
+                <el-checkbox
+                  v-else-if="modelValue.type === 'checkbox'"
+                  :model-value="isDefaultSelected(option.value)"
+                  @change="toggleDefaultValue(option.value)"
+                  size="small"
+                >
+                </el-checkbox>
+              </div>
+              
+              <!-- 选项文本输入 -->
               <el-input
                 v-model="option.label"
                 placeholder="选项文本"
                 size="small"
+                class="flex-1"
               />
+              
+              <!-- 选项值输入 -->
               <el-input
                 v-model="option.value"
                 placeholder="选项值"
                 size="small"
+                class="flex-1"
+                @input="updateOptionValue(index, $event)"
               />
-              <el-button @click="removeOption(index)" size="small">
+              
+              <!-- 删除按钮 -->
+              <el-button @click="removeOption(index)" size="small" class="flex-shrink-0">
                 <el-icon><Delete /></el-icon>
               </el-button>
             </div>
-            <el-button @click="addOption" size="small" class="w-full"
-              >添加选项</el-button
-            >
+            
+            <!-- 添加选项按钮 -->
+            <el-button @click="addOption" size="small" class="w-full">
+              添加选项
+            </el-button>
+            
+            <!-- 默认值提示 -->
+            <div class="text-xs text-gray-500 mt-2">
+              <template v-if="modelValue.type === 'radio' || modelValue.type === 'select'">
+                点击前面的圆圈选择默认选中项
+              </template>
+              <template v-else-if="modelValue.type === 'checkbox'">
+                勾选前面的方框设置默认选中项（可多选）
+              </template>
+            </div>
           </div>
         </el-form-item>
       </template>
@@ -118,9 +159,10 @@ const addOption = () => {
     if (!props.modelValue.options) {
       props.modelValue.options = [];
     }
+    const newIndex = props.modelValue.options.length + 1;
     props.modelValue.options.push({
-      label: `选项${props.modelValue.options.length + 1}`,
-      value: `${props.modelValue.options.length + 1}`,
+      label: `选项${newIndex}`,
+      value: `${newIndex}`,
     });
   }
 };
@@ -128,7 +170,73 @@ const addOption = () => {
 // 删除选项
 const removeOption = (index: number) => {
   if (props.modelValue && ['select', 'radio', 'checkbox'].includes(props.modelValue.type)) {
+    const removedOption = props.modelValue.options[index];
     props.modelValue.options.splice(index, 1);
+    
+    // 如果删除的选项是默认值，需要清除默认值
+    if (props.modelValue.type === 'checkbox') {
+      if (Array.isArray(props.modelValue.defaultValue)) {
+        const valueIndex = props.modelValue.defaultValue.indexOf(removedOption.value);
+        if (valueIndex > -1) {
+          props.modelValue.defaultValue.splice(valueIndex, 1);
+        }
+      }
+    } else if (props.modelValue.defaultValue === removedOption.value) {
+      props.modelValue.defaultValue = '';
+    }
+  }
+};
+
+// 设置默认值（单选类型）
+const setDefaultValue = (value: string) => {
+  props.modelValue.defaultValue = value;
+};
+
+// 切换默认值（多选类型）
+const toggleDefaultValue = (value: string) => {
+  if (!Array.isArray(props.modelValue.defaultValue)) {
+    props.modelValue.defaultValue = [];
+  }
+  
+  const index = props.modelValue.defaultValue.indexOf(value);
+  if (index > -1) {
+    // 如果已选中，则取消选中
+    props.modelValue.defaultValue.splice(index, 1);
+  } else {
+    // 如果未选中，则添加到选中列表
+    props.modelValue.defaultValue.push(value);
+  }
+};
+
+// 检查是否默认选中（多选类型）
+const isDefaultSelected = (value: string) => {
+  if (!Array.isArray(props.modelValue.defaultValue)) {
+    return false;
+  }
+  return props.modelValue.defaultValue.includes(value);
+};
+
+// 更新选项值时，同步更新默认值
+const updateOptionValue = (index: number, newValue: string) => {
+  const oldValue = props.modelValue.options[index].value;
+  props.modelValue.options[index].value = newValue;
+  
+  // 更新默认值中的对应值
+  if (props.modelValue.type === 'checkbox') {
+    if (Array.isArray(props.modelValue.defaultValue)) {
+      const valueIndex = props.modelValue.defaultValue.indexOf(oldValue);
+      if (valueIndex > -1) {
+        props.modelValue.defaultValue[valueIndex] = newValue;
+      }
+    }
+  } else if (props.modelValue.defaultValue === oldValue) {
+    props.modelValue.defaultValue = newValue;
   }
 };
 </script>
+
+<style scoped>
+.space-y-2 > * + * {
+  margin-top: 0.5rem;
+}
+</style>
